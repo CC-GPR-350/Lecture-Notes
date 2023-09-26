15cd6573f93da4c5d65481435a27cf0b6887331d
sabarrett <scottbarrett99@gmail.com>
2023-09-25 20:00:32 -0400

    Add images and mathjax to website.

--- publish-script.el
+++ publish-script.el
@@ -1,7 +1,16 @@
 (require 'org)
 
-(org-publish
-           '("website"
-              :base-directory "."
-              :publishing-function org-html-publish-to-html
-              :publishing-directory "./public_html"))
+(setq org-publish-project-alist
+ '(("website"
+    :base-directory "."
+    :base-extension "org"
+    :html-mathjax-options (path . "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js")
+    :publishing-function org-html-publish-to-html
+    :publishing-directory "./public_html")
+ ("images"
+   :base-directory "."
+   :base-extension "png"
+   :publishing-function org-publish-attachment
+   :publishing-directory "./public_html")))
+
+(org-publish-all t)
